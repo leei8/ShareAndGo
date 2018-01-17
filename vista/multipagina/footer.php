8d950a6406d02ad59b8b8c46b9e854adc17d8ceb
barrutieta <barrutieta@192.168.32.101>
2018-01-17 09:10:58 +0100

    registrar coche hecho (falta corregir el combo de marca)

--- vista/multipagina/footer.php
+++ vista/multipagina/footer.php
@@ -6,7 +6,7 @@
                     <img src="../img/logo.png" class="" style="height: 100px">
                 </div>
                 <div class="col-sm-2">
-                    
+
                     <ul style="list-style:none">
                         <label>Share&Go</label>
                         <li><a href="#">Sobre nosotros</a></li>
@@ -16,28 +16,39 @@
                 </div>
 
                 <div class="col-sm-3">
-                     
+
                     <ul style="list-style:none">
                         <label>Información legal</label>
                         <li><a href="#">Condiciones Generales de Uso</a></li>
                         <li><a href="#">Política de privacidad</a></li>
                         <li><a href="#">Política de cookies</a></li>
                     </ul>
-               
+
 
                 </div>
                 <div class="col-sm-2">
-                     <label>Patrocinadores</label>
-                     <img src="../img/fp_zornotza.jpg" alt="" width="85%"/>
+                    <label>Patrocinadores</label>
+                    <img src="../img/fp_zornotza.jpg" alt="" width="85%"/>
                 </div>
                 <div class="col-sm-2">
-                    <label>Redes sociales</label>
-                   FALTA LA IMAGEN CON ELLAS
+                    <label>Redes sociales</label><br>
+                    <a href="https://facebook.com/" class="btn_red_social">
+                        <img class="icono" src="../img/icono_facebook.png" alt="" id="icono"/>
+                    </a>
+                    <a href="https://www.twitter.com/" class="btn_red_social">
+                        <img class="icono" src="../img/icono_twitter.png" alt="" id="icono"/>
+                    </a>
+                    <a href="https://www.instagram.com/" class="btn_red_social">
+                        <img class="icono"src="../img/icono_instagram.png" alt="" id="icono"/>
+                    </a>
+                    <a href="https://www.youtube.com/" class="btn_red_social">
+                        <img class="icono"src="../img/icono_youtube.png" alt="" id="icono"/>
+                    </a>
                 </div>
             </div>
             <br/>
             <div class="derechos">
-                Share&Go S.L. 2005-2017. TODOS LOS DERECHOS RESERVADOS ©
+                Share&Go S.L. 2005-2017. TODOS LOS DERECHOS RESERVADOS © con el apoyo de ekothazi group S.L. ®
             </div>
         </div>
     </div>

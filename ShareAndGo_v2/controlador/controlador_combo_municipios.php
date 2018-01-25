48a1eec6655d561b0aefea0ac79846e7a34ba576
Leire <leibla8@hotmail.com>
2018-01-25 13:49:26 +0100

    Delete controlador_combo_municipios.php

--- ShareAndGo_v2/controlador/controlador_combo_municipios.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once '../modelo/modelo_municipios.php';
-$id_comunidad = 16;
-$id_municipio = NULL;
-$cont=new modelo_municipios();
-$datos=$cont->cargar_municipios($id_comunidad, $id_municipio);
-
-$municipios = json_encode($datos);
-print $municipios;  
